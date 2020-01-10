fc4b948386bd55c28e565bf9138d0692ef94ebe7
kkerekes84 <44867697+kkerekes84@users.noreply.github.com>
2020-01-10 13:11:28 +0200

    Update analog_temp.py
    cleaned up code

--- analog_temp.py
+++ analog_temp.py
@@ -1,10 +1,4 @@
- #Simple example of reading the MCP3008 analog input channels and printing
-# them all out.
-# Author: Tony DiCola
-# License: Public Domain
 import time
-
-# Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import spidev
@@ -19,10 +13,6 @@ MOSI = 10
 CS   = 8
 mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
-# Hardware SPI configuration:
-# SPI_PORT   = 0
-# SPI_DEVICE = 0
-# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 def readadc(adcnum):
     if ((adcnum > 7) or (adcnum < 0)):
@@ -30,24 +20,21 @@ def readadc(adcnum):
     r = spi.xfer2([1,(8+adcnum)<<4,0])
     adcout = ((r[1]&3) << 8) + r[2]
     return adcout
-print('Reading MCP3008 values, press Ctrl-C to quit...')
+print('Reading Analog Temperature sensor values values, press Ctrl-C to quit...')
 
 # Main program loop.
 while True:
     
-        print "Analog port 0"
         print "---------------------------"
+		print "Analog port 0"
         analog0_value = mcp.read_adc(0)
         voltage = analog0_value * 3.3
         voltage /= 1024.0
         tempCelsius = (voltage-0.5)*100
         print "ADC: ", analog0_value
-        print "---------------------------"
         print "Voltage: ", voltage
-        print "---------------------------"
         print "Temp: ", tempCelsius
         
         time.sleep(0.5)
         
- 
 
