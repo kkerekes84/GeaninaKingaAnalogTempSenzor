42e619630d555d58fa73a278fb2a07b2702061f9
kkerekes84 <44867697+kkerekes84@users.noreply.github.com>
2020-01-12 09:06:55 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,6 +4,8 @@ import analog_temp
 class TestTemp(unittest.TestCase):
       def test_temp(self):
           self.assertEqual(analog_temp.readadc(0))
+          self.assertEqual(analog_temp.readadc(1))
+          self.assertEqual(analog_temp.readadc(2))  
 
 if __name__=="__main__":
     unittest.main()
