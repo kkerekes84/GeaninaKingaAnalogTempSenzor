1da881088e4fad23e4ba7b5ef6fb4a2d2e54c474
kkerekes84 <44867697+kkerekes84@users.noreply.github.com>
2020-01-12 11:21:25 +0200

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,11 +1,11 @@
 import unittest
 import analog_temp
 
-class TestTemp(unittest.TestCase):
-      def test_temp_port0(self):
-          self.assertGreaterEqual(analog_temp.mcp.read_adc(0),200)
+class TestTempPort0(unittest.TestCase):
       def test_temp_port1(self):
           self.assertGreaterEqual(analog_temp.mcp.read_adc(1),200)
+      def test_temp_port0(self):
+          self.assertGreaterEqual(analog_temp.mcp.read_adc(0),200)
            
 
 if __name__=="__main_":
