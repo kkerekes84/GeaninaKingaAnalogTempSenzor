aef0517d9b06cf4beba5e0a11412bd254bdc45d8
GeaninaZ <46654743+GeaninaZ@users.noreply.github.com>
2020-01-12 09:57:44 +0200

    Update test2.py
    unit test update

--- test2.py
+++ test2.py
@@ -3,9 +3,8 @@ import analog_temp
 
 class TestTemp(unittest.TestCase):
       def test_temp(self):
-          self.assertEqual(analog_temp.readadc(0))
-          self.assertEqual(analog_temp.readadc(1))
-          self.assertEqual(analog_temp.readadc(2))  
+          self.assertGreaterEqual(analog_temp.mcp.read_adc(0),200)
+           
 
-if __name__=="__main__":
+if _name__=="__main_":
     unittest.main()
