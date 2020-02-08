01afdd3d3ba1b1165d0d520494f00d895ae66c0e
kkerekes84 <44867697+kkerekes84@users.noreply.github.com>
2020-02-08 15:33:53 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-import unittest
-import analog_temp_senzor_mqtt
 
-class TestTemp(unittest.TestCase):
-      def test_temp(self):
-          self.assertEqual(analog_temp_senzor_mqtt.read_temp_in_Celsius(3))
-
-if __name__=="__main__":
-    unittest.main()
