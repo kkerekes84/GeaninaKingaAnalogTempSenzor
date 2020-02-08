1ad01c29b18b784b05ba06c48966aa05e0385917
kkerekes84 <44867697+kkerekes84@users.noreply.github.com>
2020-02-08 15:34:28 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 
+import unittest
+import analog_temp_senzor_mqtt
+
+class TestTempPort(unittest.TestCase):
+      def test_temp_port(self):
+          self.assertGreaterEqual(analog_temp.mcp.read_adc(0),200)
+           
+
+if __name__=="__main_":
+    unittest.main()
